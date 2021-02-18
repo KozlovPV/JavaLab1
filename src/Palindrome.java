e4e49391351b56c13b345be3f3f45728d6928fd1
KozlovPV <X1psta24@gmail.com>
2021-02-19 00:39:54 +0300

    Polindrom2

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,49 +1,29 @@
 import javax.print.attribute.standard.PresentationDirection;
 
-/**
- * TODO:  Комментарии с описанием класса
- */
 public class Palindrome {
-    /**
-     * TODO:  Комментарии с описанием метода
-     */
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
-            String s1=reverseString(s);
-            boolean temp=true;
-            temp=isPalindrome(s,s1);
-            if(temp==true){
-                System.out.println("Слово является полиндромом!");
+            String s1 = reverseString(s);
+            boolean temp = true;
+            temp = isPalindrome(s, s1);
+            if (temp == true) {
+                System.out.println(s + " Слово является полиндромом!");
+            } else {
+                System.out.println(s + " Слово не явлеятся полиндромом!");
             }
-            else{
-                System.out.println("Слово не явлеятся полиндромом!");
-            }
-            // TODO:  определить палиндром ли это и вывести на консоль
-            // результат.  Поместите s в двойные кавычки при выводе
-            // на консоль.
         }
     }
 
     public static String reverseString(String s) {
         String str = "";
-        for (int i = s.length()-1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             str += s.charAt(i);
         }
-        System.out.println(str);
         return str;
     }
 
     public static boolean isPalindrome(String s, String s1) {
-        boolean a = false;
-
-        if (s1.equals(s) == true) {
-            a = true;
-        } else {
-            a = false;
-        }
-        return a;
+        return s1.equals(s);
     }
-
-
 }
